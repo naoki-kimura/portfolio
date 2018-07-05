a6f50cc9465032033adcbebe639e2a95c59bbf2f
木村　直暉 <noah@kimuratadashiakiranoMacBook-ea.local>
2018-07-05 16:04:50 +0900

    home page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Tripstudio"
+  end
+
+
+end
