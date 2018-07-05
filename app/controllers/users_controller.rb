545801680b5f6ae159ebcf80f04f4a99398b2fca
木村　直暉 <noah@kimuratadashiakiranoMacBook-ea.local>
2018-07-05 19:24:02 +0900

    heroku test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
 end
